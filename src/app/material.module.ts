a095a06e6ed438f8f3546e7fbab6f69f1828fb82
Alex Osman <osman@quorum.us>
2018-03-22 07:34:01 -0400

    start of main page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   MatButtonModule,
-  MatCardModule
+  MatCardModule,
+  MatIconModule,
+  MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule
   ],
 })
 export class MaterialModule { }
